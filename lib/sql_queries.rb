1684ea5833e9ef3d33495238cee6d5ee1484a22e
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-20 15:30:26 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -18,7 +18,7 @@ end
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
   "SELECT users.name, users.age, pledges.pledge_sum
   FROM users
-  INNER JOIN (SELECT SUM(amount) AS pledge_sum from pledges) pledges
+  INNER JOIN (SELECT SUM(amount) AS pledge_sum from pledges GROUP BY user_id) pledges
   ON users.id = pledges.user_id
   ORDER BY users.name
   "
