9af5d2c050a5859f6961a92a3bf40547834f99ab
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-20 16:21:12 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -44,7 +44,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
   FROM users
   INNER JOIN (SELECT user_id, amount from pledges GROUP BY user_id) pledges
   ON users.id = pledges.user_id
-  ORDER BY SUM(pledges.amount)""
+  ORDER BY SUM(pledges.amount)"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
