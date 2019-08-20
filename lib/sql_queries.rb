869d7f6ed5bb88a3d1fcc21698cda50344a23f58
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-20 15:27:42 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
   "SELECT users.name, users.age, pledges.amount
   FROM users
   INNER JOIN (SELECT amount from pledges) pledges
-  ORDER BY user.name
+  ORDER BY users.name
   "
 end
 
