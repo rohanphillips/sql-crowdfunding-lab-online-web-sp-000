aaf14afd0f5d82109ccee9c88eb04970a8c454a4
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-20 14:49:20 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,2 @@
 CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal FLOAT, start_date STRING, end_date STRING);
+CREATE TABLE users(id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
