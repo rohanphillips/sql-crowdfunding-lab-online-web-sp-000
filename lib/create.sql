bf18029c5c392eb6daf81c86afc13a4e4bd1288b
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-20 14:48:09 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,1 @@
-CREATE TABLE groceries (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal FLOAT, start_date STRING, end_date STRING);
+CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal FLOAT, start_date STRING, end_date STRING);
