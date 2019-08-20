5ad370ec6a95357a0747624aed7c3cffc42ecdfb
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-20 14:45:05 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,5 +1,1 @@
-CREATE TABLE groceries (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER, aisle INTEGER);
-
-INSERT INTO groceries VALUES (1, "Apples", 2, 1);
-INSERT INTO groceries VALUES (2, "Oranges", 4, 2);
-INSERT INTO groceries VALUES(3, "Peaches", 6, 3);
+CREATE TABLE groceries (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal FLOAT, start_date STRING, end_date STRING);
