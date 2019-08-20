991ed2b7477d8c56fc276f748992d2783865e992
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-20 16:31:30 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -51,6 +51,7 @@ end
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   "SELECT projects.category, pledges.amount
   FROM projects
+  WHERE projects.category = 'music'
   INNER JOIN pledges
   ON projects.id = pledges.project_id
   "
