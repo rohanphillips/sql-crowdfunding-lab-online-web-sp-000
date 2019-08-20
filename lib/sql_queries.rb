5065864de1a1dda189119968ff7213f6edf27c80
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-20 16:20:26 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -34,12 +34,17 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
-  "SELECT users.name, pledges.pledge_sum
+  # "SELECT users.name, pledges.pledge_sum
+  # FROM users
+  # INNER JOIN (SELECT user_id, SUM(Amount) AS pledge_sum from pledges GROUP BY user_id) pledges
+  # ON users.id = pledges.user_id
+  # ORDER BY pledges.pledge_sum
+  #"
+  SELECT users.name, SUM(pledges.amount)
   FROM users
-  INNER JOIN (SELECT user_id, SUM(Amount) AS pledge_sum from pledges GROUP BY user_id) pledges
+  INNER JOIN (SELECT user_id, amount from pledges GROUP BY user_id) pledges
   ON users.id = pledges.user_id
-  ORDER BY pledges.pledge_sum
-  "
+  ORDER BY SUM(pledges.amount)
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
